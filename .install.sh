3cae102b02aacb9d9c913cbd773204a11ed4d247
volkovasystems <richeve.bebedor@gmail.com>
2017-01-25 11:57:02 +0800

    Fixed depenendency.

---
+++ .install.sh
@@ -0,0 +1,31 @@
+yarn add \
+babel-runtime \
+clazof \
+doubt \
+falze \
+falzy \
+harden \
+protype \
+raze \
+truu \
+zelf \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+clazof \
+doubt \
+falze \
+falzy \
+harden \
+protype \
+raze \
+truu \
+zelf \
+--save
